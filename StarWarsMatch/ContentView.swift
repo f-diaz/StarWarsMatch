74677b1787b773888362ec4dba5834034e67b85d
Fernando Diaz <hola@fernandodiaz.dev>
2024-07-19 22:34:16 +0200

    Initial commit

---
+++ StarWarsMatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StarWarsMatch
+//
+//  Created by Fernando Diaz de Tudela on 19/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StarWarsMatch/StarWarsMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsMatchApp.swift
+//  StarWarsMatch
+//
+//  Created by Fernando Diaz de Tudela on 19/7/24.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
